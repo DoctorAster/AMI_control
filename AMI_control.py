ee8c246e240a94df46648faf686a47783ddc9818
doctor.connect <connectkz.com@gmail.com>
2023-07-29 00:10:21 +0600

    def login begin

--- AMI_control.py
+++ AMI_control.py
@@ -4,3 +4,12 @@ import telnetlib
 import time
 import mysql.connector
 
+
+def login (action_id, username = 'admin', password = 'amp111'):
+    ami_resp = b'Asterisk Call Manager/'
+    output = telnetObj.read_until(ami_resp)
+    command_login = 'Login'
+    action_id += 1
+
+
+
